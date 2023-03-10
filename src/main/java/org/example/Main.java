7835338615678acfa63d394f7448e3eeee93142c
VictIl <orange9table9@gmail.com>
2023-03-11 00:49:11 +0100

    descriptive message

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.Scanner;
+public class Main {
+        static int factorial(int n){
+            if (n == 0)
+                return 1;
+            else
+                return(n * factorial(n-1));
+        }
+        public static void main(String args[]){
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Number: ");
+            int num=myObj.nextInt();
+            System.out.println(factorial(num));
+        }
+
+}
